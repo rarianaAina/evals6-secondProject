c429c1058c9247d2eabad49f1ff15e8e183b3efc
Web dev <141223950+rarianaAina@users.noreply.github.com>
2025-03-22 11:01:47 +0300

    First commit

---
+++ src/main/java/com/crm/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.crm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Bienvenue sur la page d'accueil !");
+        return "home"; // Redirige vers home.html
+    }
+}
+
